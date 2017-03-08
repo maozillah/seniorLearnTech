ea51580cbcd17b8f5f05dcb683a5b72299b4e783
maozillah <kayexmao@gmail.com>
2017-03-08 10:42:47 -0500

    Counting numbers

--- scripts/HBtemplates.js
+++ scripts/HBtemplates.js
@@ -1,9 +1,11 @@
+Handlebars.registerHelper("counter", function (index){
+    return index + 1;
+});
+
 // asynchronous call
-
-
 $.getJSON("../surveyData.json", function(json) {
     
-    var template = $('#handlebars-demo').html();
+    var template = $('#survey-ques').html();
 
     var context = json;
 
