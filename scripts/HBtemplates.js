c214ff40fafd97eb3fda51e9f4dbe85773aecb9d
maozillah <kayexmao@gmail.com>
2017-03-08 17:35:15 -0500

    Relinking Json

--- scripts/HBtemplates.js
+++ scripts/HBtemplates.js
@@ -2,8 +2,11 @@ Handlebars.registerHelper("counter", function (index){
     return index + 1;
 });
 
+var jsonData = "https://maozillah.github.io/seniorLearnTech/surveyData.json";
+// var jsonData = "../surveyData.json";
+
 // asynchronous call
-$.getJSON("../surveyData.json", function(json) {
+$.getJSON(jsonData, function(json) {
     
     var template = $('#survey-ques').html();
 
